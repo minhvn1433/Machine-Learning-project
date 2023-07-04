124e41e82e3d1547366d6c3a8a0655b8d12c2e25
Selina-th4 <103742905+Selina-th4@users.noreply.github.com>
2023-07-04 15:06:32 +0700

    from máy Hiền

--- parameter.py
+++ parameter.py
@@ -5,6 +5,6 @@ DATASET = 'imagenette'
 numclasses = 10
 
 CONV_DO_RATE = 0.1 #[0.05, 0.1, 0.3, 0.5]
-LINEAR_DO_RATE = 0.1 #[0.05, 0.1, 0.3, 0.5]
+LINEAR_DO_RATE = 0.5 #[0.05, 0.1, 0.3, 0.5]
 
-LEARNING_RATE = 3e-4
+LEARNING_RATE = 3e-4 #[6e-5, 3e-4, 1e-3]
